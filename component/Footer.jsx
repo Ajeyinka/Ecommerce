a8ae0c54887be6f8d4ca027228636b7cee5ffda5
Olayinka <ajeyinka12@gmail.com>
2022-11-29 19:33:59 +0100

    gggg

---
+++ component/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
+
+const Footer = () => {
+  return (
+    <div className='footer-container'>
+      <p>
+        2022 DesignerOla Estore All rights reserved
+      </p>
+      <p>
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+
+    </div>
+  )
+}
+
+export default Footer
---
+++ pages/_app.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Toaster} from 'react-hot-toast'
+import '../styles/globals.css';
+
+import { Layout } from '../component';
+import {StateContext} from '../context/StateContext';
+
+
+function MyApp({ Component, pageProps }) {
+  return ( 
+    <StateContext >
+       <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+    </Layout>
+    </ StateContext>
+    
+  
+  )
+}
+
+export default MyApp
